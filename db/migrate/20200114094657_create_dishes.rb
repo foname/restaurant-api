5fc86694b7004342c03b8d5ff854ad2dc7026cda
Fon <chanaphan.prasomwong@gmail.com>
2020-01-15 11:03:02 +0700

    remove unnescessary comments

--- db/migrate/20200114094657_create_dishes.rb
+++ db/migrate/20200114094657_create_dishes.rb
@@ -2,8 +2,6 @@ class CreateDishes < ActiveRecord::Migration[6.0]
   def change
     create_table :dishes do |t|
       t.string :name
-
-      # t.timestamps
     end
   end
 end
--- db/migrate/20200114154709_create_restaurants_dishes.rb
+++ db/migrate/20200114154709_create_restaurants_dishes.rb
@@ -3,8 +3,6 @@ class CreateRestaurantsDishes < ActiveRecord::Migration[6.0]
     create_table :restaurants_dishes do |t|
       t.references :restaurant, null: false, foreign_key: true
       t.references :dish, null: false, foreign_key: true
-
-      # t.timestamps
     end
   end
 end
